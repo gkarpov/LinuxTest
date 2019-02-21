4f484e58b760080e68715fc33d1d52bf28139692
Georgi Karpov <karpov21g@gmail.com>
2019-02-22 00:51:32 +0200

    Renamed Projects

--- AJAXTests/Properties/AssemblyInfo.cs
+++ AJAXTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AJAXTests")]
+[assembly: AssemblyTitle("ClassLibrary1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AJAXTests")]
+[assembly: AssemblyProduct("ClassLibrary1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bafb2767-ec08-4510-847d-7ddccd9dcf65")]
+[assembly: Guid("98070c8e-466b-4261-b5b0-deb60f81d427")]
 
 // Version information for an assembly consists of the following four values:
 //
