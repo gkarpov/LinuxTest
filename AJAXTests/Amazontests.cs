3174d1e6eba6de9202360cdd0f00d937f9b02477
Georgi Karpov <karpov21g@gmail.com>
2019-02-22 00:48:44 +0200

    First commit

---
+++ AJAXTests/Amazontests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonTest
+{
+    [TestFixture]
+    public class Amazontests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Url = "www.amazon.com";
+
+        }
+
+        [Test(Description = "Verify item in chart")]
+        public void ItemInChart(){
+
+            
+        }
+
+
+    }
+}
