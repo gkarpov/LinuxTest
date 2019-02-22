785ad62dcb06b8dccc7866c679f5cbd6f3268439
Georgi Karpov <karpov21g@gmail.com>
2019-02-22 03:20:21 +0200

    update tests

--- AJAXTests/AJAXReqestTests.cs
+++ AJAXTests/AJAXReqestTests.cs
@@ -21,6 +21,23 @@ namespace AJAXTests
         }
 
 
+        //TODO
+        //[TestCase("nl", "3825 ", HttpStatusCode.OK, TestName = "Check status code for NL zip code 7411")]
+        //[TestCase("lv", "1050 ", HttpStatusCode.NotFound, TestName = "Check status code for LV zip code 1050"]
+        //public void StatusCodeTest(string countryCode, string zipCode, HttpStatusCode expectedHttpStatusCode)
+        //{
+        //    // arrange
+        //    RestClient client = new RestClient(" http://api.zippopotam.us");
+        //    RestRequest request = new RestRequest($"
+        //    { countryCode}/{ zipCode}
+        //    ", Method.GET);
+
+        //    // act
+        //    IRestResponse response = client.Execute(request);
+
+        //    // assert
+        //    Assert.That(response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+        //}
 
         [Test]
         public void StatusCodeTest_GET_SINGLE_USER()
